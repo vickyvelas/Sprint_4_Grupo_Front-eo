3191037ede248cf14a9cbe9dd1c91d29d88016c5
Dolores Jadur <121775823+dolojadur@users.noreply.github.com>
2023-11-06 10:32:02 -0300

    commit rol

--- src/main/java/com/utn/sprint_4/entidades/Usuario.java
+++ src/main/java/com/utn/sprint_4/entidades/Usuario.java
@@ -1,5 +1,6 @@
 package com.utn.sprint_4.entidades;
 
+import com.utn.sprint_4.enumeraciones.Rol;
 import jakarta.persistence.*;
 import lombok.*;
 import org.antlr.v4.runtime.misc.NotNull;
@@ -35,5 +36,9 @@ public class Usuario extends Base {
     @Column(name = "fecha_baja")
     @Temporal(TemporalType.TIMESTAMP)
     private Date fechaBaja;
+
+    @Column(name = "rol")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Rol rol;
 
 }
