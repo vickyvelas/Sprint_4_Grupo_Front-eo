97ebd594483e0a33faa4efab66778b7d2691ec4d
pabloyacante <143475348+pabloyacante@users.noreply.github.com>
2023-11-07 18:55:59 -0300

    Update FacturaServiceImpl.java

--- src/main/java/com/utn/sprint_4/servicios/FacturaServiceImpl.java
+++ src/main/java/com/utn/sprint_4/servicios/FacturaServiceImpl.java
@@ -1,6 +1,7 @@
 package com.utn.sprint_4.servicios;
 
 import com.utn.sprint_4.entidades.Factura;
+import com.utn.sprint_4.enumeraciones.FormaPago;
 import com.utn.sprint_4.repositorios.BaseRepository;
 import com.utn.sprint_4.repositorios.FacturaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 @Service
@@ -17,15 +19,15 @@ public class FacturaServiceImpl extends BaseServiceImpl<Factura,Long> implements
     private FacturaRepository facturaRepository;
 
 
-    public FacturaServiceImpl(BaseRepository<Factura, Long> baseRepository) {
+    public FacturaServiceImpl(BaseRepository<Factura, Long> baseRepository, FacturaRepository facturaRepository) {
         super(baseRepository);
         this.facturaRepository = facturaRepository;
     }
 
     @Override
-    public List<Factura> search(String filtro) throws Exception {
+    public List<Factura> search(String paymenttype, String preferenceid, Long paymentid, BigDecimal totalventa, FormaPago formapago, Long merchantorder) throws Exception {
         try{
-            List<Factura> facturas = facturaRepository.searchNativo(filtro);
+            List<Factura> facturas = facturaRepository.searchNativo(paymenttype,preferenceid,paymentid,totalventa,formapago,merchantorder);
             return facturas;
         }catch (Exception e){
             throw new Exception(e.getMessage());
@@ -33,9 +35,9 @@ public class FacturaServiceImpl extends BaseServiceImpl<Factura,Long> implements
     }
 
     @Override
-    public Page<Factura> search(String filtro, Pageable pageable) throws Exception {
+    public Page<Factura> search(String paymenttype, String preferenceid, Long paymentid, BigDecimal totalventa, FormaPago formapago,Long merchantorder, Pageable pageable) throws Exception {
         try{
-            Page<Factura> facturas = facturaRepository.searchNativo(filtro, pageable);
+            Page<Factura> facturas = facturaRepository.searchNativo(paymenttype,preferenceid,paymentid,totalventa,formapago,merchantorder, pageable);
             return facturas;
         }catch (Exception e){
             throw new Exception(e.getMessage());
