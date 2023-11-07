dd65d634a647158c2a9e43c95ec37ff219573663
pabloyacante <143475348+pabloyacante@users.noreply.github.com>
2023-11-07 18:55:36 -0300

    Update FacturaService.java

--- src/main/java/com/utn/sprint_4/servicios/FacturaService.java
+++ src/main/java/com/utn/sprint_4/servicios/FacturaService.java
@@ -1,15 +1,19 @@
 package com.utn.sprint_4.servicios;
 
 import com.utn.sprint_4.entidades.Factura;
+import com.utn.sprint_4.enumeraciones.FormaPago;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.query.Param;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 public interface FacturaService extends BaseService<Factura, Long>{
 
-    List<Factura> search(String filtro) throws Exception;
+    List<Factura> search(String paymenttype, String preferenceid, Long paymentid, BigDecimal totalventa, FormaPago formapago,Long merchantorder) throws Exception;
 
-    Page<Factura> search(String filtro, Pageable pageable) throws Exception;
+    Page<Factura> search(String paymenttype, String preferenceid, Long paymentid,BigDecimal totalventa, FormaPago formapago,Long merchantorder, Pageable pageable) throws Exception;
 
 }
+
