ab8ae892f854398021dfc8e5e69959a9fd72761d
Agustina Linares <143526107+aguslinares@users.noreply.github.com>
2023-11-10 09:45:25 -0300

    .

--- src/main/java/com/utn/sprint_4/dtos/BusquedaProductosDTO.java
+++ src/main/java/com/utn/sprint_4/dtos/BusquedaProductosDTO.java
@@ -9,15 +9,15 @@ import lombok.Data;
 public class BusquedaProductosDTO {
 
     private String denominacion;
-
     private String descripcion;
-
-    private Integer tiempoEstimadoCocina;
-
     private int precioVenta;
-
-    private int costo;
-
     private String urlImagen;
+
+    public BusquedaProductosDTO (String denominacion, String descripcion, int precioVenta, String urlImagen){
+        this.denominacion = denominacion;
+        this.descripcion = descripcion;
+        this.precioVenta = precioVenta;
+        this.urlImagen = urlImagen;
+    }
 
 }
